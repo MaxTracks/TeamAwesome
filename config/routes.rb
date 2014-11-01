2d4674c363f4fed3ad923e6ef6137c0c041363db
Peter <Peter>
2014-11-01 17:23:13 -0400

    add authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   root 'static_pages#home'
 
   get 'about', to: 'static_pages#about'
   get 'signup', to: 'users#new'
 
   resources :users
+
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
 
 end
